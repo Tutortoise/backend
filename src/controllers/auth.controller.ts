f87de68bf46af68a03049fe243e6f80e7ca563ad
Faidil <faidilf@proton.me>
2024-11-12 23:33:52 +0800

    feat(register): add simple register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,36 @@
 import { Request, Response } from "express";
+import { registerSchema } from "../schemas/auth.schema";
+import { auth } from "../config";
+import { z } from "zod";
+import type { Controller } from "../types";
 
 export const helloAuth = (_req: Request, res: Response) => {
   res.json({ message: "hello auth" });
+};
+
+type RegisterSchema = z.infer<typeof registerSchema>;
+export const register: Controller<RegisterSchema> = async (req, res) => {
+  try {
+    const { name: displayName, email, password } = req.body;
+
+    const user = await auth.createUser({ displayName, email, password });
+
+    res.status(201).json({
+      status: "success",
+      message: "register success",
+      data: { userId: user.uid },
+    });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      res.status(400).json({
+        status: "fail",
+        message: "validation error",
+        errors: error.message,
+      });
+    }
+
+    if (error instanceof Error) {
+      res.status(400).json({ status: "fail", message: error.message });
+    }
+  }
 };
--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import * as authController from "@controllers/auth.controller";
+import { validator } from "../../middleware/validation.middleware";
+import { registerSchema } from "../../schemas/auth.schema";
 
 // /api/v1/auth
 const authRouter = Router();
 
 authRouter.get("/", authController.helloAuth);
+
+authRouter.post(
+  "/register",
+  validator(registerSchema),
+  authController.register,
+);
 
 export default authRouter;
