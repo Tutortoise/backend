1e67601d6f04875c5a58fae1e6ba947c36a2cb10
Veirt <veirt@duck.com>
2024-11-13 05:03:22 +0800

    feat(user/profile): add update user profile endpoint
    - make all the body optional

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,32 @@
+import { z } from "zod";
+import { userSchema } from "../schemas/user.schema";
 import { Controller } from "../types";
+import { firestore } from "../config";
+import { logger } from "../middleware/logging.middleware";
 
-export const updateProfile: Controller = async (req, res) => {
-  // TODO
-  res.json();
+type UpdateProfileSchema = z.infer<typeof userSchema>;
+export const updateProfile: Controller<UpdateProfileSchema> = async (
+  req,
+  res,
+) => {
+  try {
+    firestore
+      .collection("users")
+      .doc(req.user.id)
+      .update({
+        ...req.body,
+        updatedAt: new Date(),
+      });
+  } catch (error) {
+    logger.debug("Failed to update profile", error);
+
+    res
+      .status(500)
+      .json({ status: "error", message: "Failed to update profile" });
+    return;
+  }
+
+  res
+    .status(200)
+    .json({ status: "success", message: "User profile updated successfully" });
 };
--- src/routes/v1/user.route.ts
+++ src/routes/v1/user.route.ts
@@ -4,12 +4,18 @@ import {
   firebaseAuthMiddleware,
   verifyUser,
 } from "../../middleware/auth.middleware";
+import { validator } from "../../middleware/validation.middleware";
+import { userSchema } from "../../schemas/user.schema";
 
 // /api/v1/users
 const userRouter = Router();
 userRouter.use(firebaseAuthMiddleware);
 userRouter.use(verifyUser);
 
-userRouter.patch("/profile", userController.updateProfile);
+userRouter.patch(
+  "/profile",
+  validator(userSchema),
+  userController.updateProfile,
+);
 
 export default userRouter;
