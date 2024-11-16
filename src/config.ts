8acda7c83786a282b3dceaa0b09abf8f7206f38e
Veirt <veirt@duck.com>
2024-11-17 04:03:03 +0800

    fix(config): prevent Firebase errors in test environment

--- src/config.ts
+++ src/config.ts
@@ -13,20 +13,28 @@ if (!GCS_BUCKET_NAME) {
 
 // Firebase Related
 if (
-  !process.env.FIREBASE_SERVICE_ACCOUNT_KEY ||
-  !process.env.FIREBASE_DATABASE_URL
+  process.env.NODE_ENV !== "test" &&
+  (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY ||
+    !process.env.FIREBASE_DATABASE_URL)
 ) {
   throw new Error("Missing Firebase Service Account Key or Database URL");
 }
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string,
-);
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+  : undefined;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-});
+let options: admin.AppOptions;
+if (process.env.NODE_ENV === "test") {
+  options = { projectId: "tutortoise-test" };
+} else {
+  options = {
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  };
+}
+
+admin.initializeApp(options);
 
 export const auth = admin.auth();
 export const firestore = admin.firestore();
