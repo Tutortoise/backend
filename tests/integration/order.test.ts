0c8b845c3605f0f8aaf2ce1c20b5dc3bb164c6c2
Veirt <veirt@duck.com>
2024-11-22 04:15:46 +0800

    test(tutorservice): accept and decline integration testing

--- tests/integration/order.test.ts
+++ tests/integration/order.test.ts
@@ -8,6 +8,15 @@ import { afterAll, describe, expect, test } from "vitest";
 
 const tsService = new TutorServiceService({ firestore });
 
+async function cleanupOrders() {
+  const snapshot = await firestore.collection("orders").get();
+  const batch = firestore.batch();
+  snapshot.docs.forEach((doc) => {
+    batch.delete(doc.ref);
+  });
+  await batch.commit();
+}
+
 async function registerLearner() {
   const newLearner = {
     name: faker.person.fullName(),
@@ -34,12 +43,7 @@ describe("Order a service", async () => {
   const services = await tsService.getTutorServices();
 
   afterAll(async () => {
-    const snapshot = await firestore.collection("orders").get();
-    const batch = firestore.batch();
-    snapshot.docs.forEach((doc) => {
-      batch.delete(doc.ref);
-    });
-    await batch.commit();
+    await cleanupOrders();
   });
 
   test("Learner can order a service", async () => {
@@ -90,6 +94,10 @@ describe("Cancel an order", async () => {
 
   const { userId, idToken: learnerIdToken } = await registerLearner();
 
+  afterAll(async () => {
+    await cleanupOrders();
+  });
+
   test("Tutor can cancel an order", async () => {
     const tutorId = await firestore
       .collection("tutor_services")
@@ -132,5 +140,93 @@ describe("Cancel an order", async () => {
       .post(`/api/v1/orders/x/cancel`)
       .set("Authorization", `Bearer ${learnerIdToken}`)
       .expect(403);
+  });
+});
+
+describe("Accept an order", async () => {
+  test("Tutor can accept an order", async () => {
+    const services = await tsService.getTutorServices();
+    const randomService = faker.helpers.arrayElement(services);
+
+    const { userId, idToken: learnerIdToken } = await registerLearner();
+
+    const tutorId = await firestore
+      .collection("tutor_services")
+      .doc(randomService.id)
+      .get()
+      .then(async (doc) => {
+        const ref = doc.data()?.tutorId;
+        return ref.id;
+      });
+    const tutorIdToken = await login(tutorId);
+
+    // Create an order
+    const availability = await tsService.getTutorServiceAvailability(
+      randomService.id,
+    );
+
+    await supertest(app)
+      .post("/api/v1/orders")
+      .set("Authorization", `Bearer ${learnerIdToken}`)
+      .send({
+        learnerId: userId,
+        tutorServiceId: randomService.id,
+        sessionTime: availability[0],
+        totalHours: 1,
+        notes: "I want to learn more",
+      })
+      .expect(201);
+
+    // Accept the order
+    const orders = await tsService.getOrders(randomService.id);
+    const order = orders[0];
+    await supertest(app)
+      .post(`/api/v1/orders/${order.id}/accept`)
+      .set("Authorization", `Bearer ${tutorIdToken}`)
+      .expect(200);
+  });
+});
+
+describe("Decline an order", async () => {
+  test("Tutor can decline an order", async () => {
+    const services = await tsService.getTutorServices();
+    const randomService = faker.helpers.arrayElement(services);
+
+    const { userId, idToken: learnerIdToken } = await registerLearner();
+
+    const tutorId = await firestore
+      .collection("tutor_services")
+      .doc(randomService.id)
+      .get()
+      .then(async (doc) => {
+        const ref = doc.data()?.tutorId;
+        return ref.id;
+      });
+    const tutorIdToken = await login(tutorId);
+
+    // Create an order
+    const availability = await tsService.getTutorServiceAvailability(
+      randomService.id,
+    );
+
+    await supertest(app)
+      .post("/api/v1/orders")
+      .set("Authorization", `Bearer ${learnerIdToken}`)
+      .send({
+        learnerId: userId,
+        tutorServiceId: randomService.id,
+        sessionTime: availability[0],
+        totalHours: 1,
+        notes: "I want to learn more",
+      })
+      .expect(201);
+
+    // Decline the order
+    const orders = await tsService.getOrders(randomService.id);
+    const order = orders[0];
+    await supertest(app)
+      .post(`/api/v1/orders/${order.id}/decline`)
+      .set("Authorization", `Bearer ${tutorIdToken}`)
+      .expect(200);
   });
 });
